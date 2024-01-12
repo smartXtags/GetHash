822adf60a7d6a7aab48e03cbf6de96b562a52071
viniciussmartx <vinicius.chaves@smartx.com.br>
2024-01-12 12:04:57 -0300

    Generate Hash

---
+++ ConsumirAPIMcDonalds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+Console.Write("Insira um valor para transformar em Hash: ");
+string input = Console.ReadLine();
+using var sha256 = SHA256.Create();
+var bytes = Encoding.UTF8.GetBytes(input);
+var hashBytes = sha256.ComputeHash(bytes);
+
+var verify = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+Console.WriteLine("Meu string criptografado: " + verify);
